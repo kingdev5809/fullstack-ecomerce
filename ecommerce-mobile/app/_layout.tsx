a48dcf26a9eeb10d8e9efff9b445a96edd5e6084
abdurashid <kingdev5809@gmail.com>
2025-02-02 15:19:21 +0500

    Some bugs fixed and some optimizations

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider mode="dark">
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
