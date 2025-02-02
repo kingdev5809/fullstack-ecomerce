b5424f96fc52ab5e8ae368b67d9fd0b9795a8ac0
abdurashid <kingdev5809@gmail.com>
2025-02-02 15:57:42 +0500

    Some elements added to product card

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,5 +1,13 @@
-import { Text, View } from "react-native";
-import { Button, ButtonText } from "./ui";
+import {
+  Text,
+  Button,
+  ButtonText,
+  Card,
+  Image,
+  VStack,
+  Heading,
+  Box,
+} from "./ui";
 
 interface Product {
   id: number;
@@ -14,11 +22,36 @@ interface ProductCardProps {
 }
 export function ProductCard({ product }: ProductCardProps) {
   return (
-    <View>
-      <Text>{product.name}</Text>
-      <Button variant="outline">
-        <ButtonText>Buy</ButtonText>
-      </Button>
-    </View>
+    <Card className="p-5 rounded-lg max-w-[360px] m-3">
+      <Image
+        source={{
+          uri: product.image,
+        }}
+        className="mb-6 h-[240px] w-full rounded-md object-cover aspect-[4/3]"
+        alt="image"
+      />
+      <Text className="text-sm font-normal mb-2 text-typography-700">
+        Fashion Clothing
+      </Text>
+      <VStack className="mb-6">
+        <Heading size="md" className="mb-4">
+          {product.name}{" "}
+        </Heading>
+        <Text size="sm">{product.description}</Text>
+      </VStack>
+      <Box className="flex-col sm:flex-row">
+        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+          <ButtonText size="sm">Add to cart</ButtonText>
+        </Button>
+        <Button
+          variant="outline"
+          className="px-4 py-2 border-outline-300 sm:flex-1"
+        >
+          <ButtonText size="sm" className="text-typography-600">
+            Wishlist
+          </ButtonText>
+        </Button>
+      </Box>
+    </Card>
   );
 }
--- ecommerce-mobile/components/ui/index.ts
+++ ecommerce-mobile/components/ui/index.ts
@@ -1,1 +1,7 @@
 export * from "./button";
+export * from "./card";
+export * from "./image";
+export * from "./text";
+export * from "./vstack";
+export * from "./heading";
+export * from "./box";
